3fe832f7d0574ccc8ba82422cb7bb9a5d7603cb2
ahmed <amhk11827@gmail.com>
2021-10-29 23:39:06 +0200

    Update

--- lib/module/Profile/profile_screen.dart
+++ lib/module/Profile/profile_screen.dart
@@ -5,8 +5,38 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Row(
+            children:[
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.grey[300],
+                child: Icon(Icons.person_outline,size: 50,color: Colors.grey,)
+              ),
+              TextButton(onPressed: (){}, child: Text('log in')),
+              Text('|'),
+              TextButton(onPressed: (){}, child: Text('Sign up'))
+            ],
+
+          ),
+          SizedBox(height: 40,),
+          ListTile(
+            leading: Icon(Icons.shopping_bag),
+            title: Text('Last orders'),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+          ListTile(
+            leading: Icon(Icons.info_outline),
+            title: Text('About'),
+          ),
+        ],
+      ),
     );
   }
 }
